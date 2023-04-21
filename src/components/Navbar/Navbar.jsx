9f10447cc85a9780259d053670352dfd2519d58e
guilherhenri <guilherhenri12@gmail.com>
2023-04-21 10:43:08 -0300

    Added navbar component structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
+import { images } from '../../constants/index';
 import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt="" />
+      </div>
+
+      <ul className='app__navbar-links'>
+        {['home', 'sobre', 'trabalho', 'habilidades', 'contato'].map((item) => (
+          <li key={`link-${item}`} className='app__flex p-text'>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
