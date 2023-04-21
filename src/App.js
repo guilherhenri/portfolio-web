9fc56c6a555893a62f0e14de56d3e9513d3f92c9
guilherhenri <guilherhenri12@gmail.com>
2023-04-21 10:21:12 -0300

    Created containers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from './containers';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   )
 }
 
