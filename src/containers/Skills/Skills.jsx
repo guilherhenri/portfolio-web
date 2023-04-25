a350b943bc89fa7b976ebf1f96248e8fbff73150
guilherhenri <guilherhenri12@gmail.com>
2023-04-25 06:30:42 -0300

    Created skills container structure

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-
+import ReactTooltip from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper'; 
 import { images } from '../../constants';
@@ -13,7 +13,44 @@ const Skills = () => {
 
       <div className='app__skills-container'>
         <motion.div className='app__skills-list'>
-          
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+            className='app__skills-item app__flex'
+          >
+            <div className="app__flex">
+              <img src={images.react} alt="img" />
+            </div>
+
+            <p className="p-text">React</p>
+
+          </motion.div>
+
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+            className='app__skills-item app__flex'
+          >
+            <div className="app__flex">
+              <img src={images.node} alt="img" />
+            </div>
+
+            <p className="p-text">Node</p>
+
+          </motion.div>
+
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+            className='app__skills-item app__flex'
+          >
+            <div className="app__flex">
+              <img src={images.typescript} alt="img" />
+            </div>
+
+            <p className="p-text">Typescript</p>
+
+          </motion.div>
         </motion.div>
       </div>
     </>
